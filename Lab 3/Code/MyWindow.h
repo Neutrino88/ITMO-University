3ebb00da88d6293278fe678ee0e6a5221323ebff
Oleg Romanov <romanov-97@mail.ru>
2017-10-11 00:12:11 +0300

    Refactor lab_3

--- Lab 3/Code/MyWindow.h
+++ Lab 3/Code/MyWindow.h
@@ -8,47 +8,9 @@ class MyWindow {
 	HWND hWnd;
 
 public:
-	MyWindow(HINSTANCE HInstance, int NCmdShow)
-	{
-		hInstance = HInstance;
-		nCmdShow = NCmdShow;
-	}
+	MyWindow(const HINSTANCE hInstance, const int nCmdShow);
 
-	HWND getHWnd() {
-		return hWnd;
-	}
-
-	int create(LRESULT (*WndProc)(HWND, UINT, UINT, LONG)) {
-		/* Set WINDOW attributes */
-		wndClass.style = CS_HREDRAW | CS_VREDRAW;
-		wndClass.lpfnWndProc = (WNDPROC)WndProc;
-		wndClass.cbClsExtra = 0;
-		wndClass.cbWndExtra = 0;
-		wndClass.hInstance = hInstance;
-		wndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-		wndClass.lpszMenuName = NULL;
-		wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-		wndClass.lpszClassName = L"window";
-
-		/* Register WINDOWS class */
-		if (!RegisterClass(&wndClass)) {
-			return 1;
-		}
-
-		/* Create WINDOW */
-		hWnd = CreateWindow(wndClass.lpszClassName,
-			L"Lab 3", WS_POPUPWINDOW | WS_CLIPCHILDREN | WS_CAPTION,
-			600, 200, 180, 290, NULL, NULL, hInstance, NULL);
-
-		if (!hWnd) {
-			return 2;
-		}
-
-		return 0;
-	}
-
-	void show() {
-		ShowWindow(hWnd, nCmdShow);
-		UpdateWindow(hWnd);
-	}
+	void show(void) const;
+	HWND getHWnd(void) const;
+	int create(LRESULT(*WndProc)(HWND, UINT, UINT, LONG));
 };
--- Lab 3/Code/main.cpp
+++ Lab 3/Code/main.cpp
@@ -12,11 +12,11 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nCmdSh
 
 	size_t x=10, y=0;
 
-	MyEdit edit1(hInstance, mainWin.getHWnd(), MYEDIT_WANTRETURN | MYEDIT_MULTILINE | MYEDIT_CENTER, x, y += 40, 140, 40);
-	MyEdit edit2(hInstance, mainWin.getHWnd(), MYEDIT_AUTOHSCROLL | MYEDIT_AUTOVSCROLL | MYEDIT_MULTILINE | MYEDIT_UPPERCASE, x, y += 40, 140, 40);
-	MyEdit edit4(hInstance, mainWin.getHWnd(), MYEDIT_LOWERCASE, x, y += 40, 140, 40);
-	MyEdit edit5(hInstance, mainWin.getHWnd(), MYEDIT_PASSWORD, x, y += 40, 140, 40);
-	MyEdit edit6(hInstance, mainWin.getHWnd(), MYEDIT_RIGHT, x, y += 40, 140, 40);
+	MyEdit edit1(hInstance, mainWin.getHWnd(), MYEDIT_AUTOHSCROLL | MYEDIT_AUTOVSCROLL | MYEDIT_MULTILINE | MYEDIT_UPPERCASE, x, y += 40, 140, 40);
+	MyEdit edit2(hInstance, mainWin.getHWnd(), MYEDIT_WANTRETURN | MYEDIT_MULTILINE | MYEDIT_CENTER, x, y += 40, 140, 40);
+	MyEdit edit3(hInstance, mainWin.getHWnd(), MYEDIT_LOWERCASE, x, y += 40, 140, 40);
+	MyEdit edit4(hInstance, mainWin.getHWnd(), MYEDIT_PASSWORD, x, y += 40, 140, 40);
+	MyEdit edit5(hInstance, mainWin.getHWnd(), MYEDIT_RIGHT, x, y += 40, 140, 40);
 
 	mainWin.show();
 
@@ -31,14 +31,9 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, LPWSTR lpCmdLine, int nCmdSh
 }
 
 static LRESULT WndProc(HWND hWnd, UINT Message, UINT wParam, LONG lParam) {
-	HANDLE hBitmap;
-	BITMAP bitmap;
-	HDC hCompatibleDC;
-
 	switch (Message) {
 		case WM_DESTROY: {
 			PostQuitMessage(0);
-			break;
 		}
 		default: {
 			return DefWindowProc(hWnd, Message, wParam, lParam);
